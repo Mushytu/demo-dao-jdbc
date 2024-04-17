57ddf3b3ca324c65d36e905cdb0bbecbd482e9bc
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2024-04-17 14:05:13 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller sll = new Seller(12, "Nelio", "nelio@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(sll);
 
 	}
 
