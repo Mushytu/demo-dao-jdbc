426bdabb37115d57dd88a520e67ebd6a0e43ba2e
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2024-04-17 14:10:21 -0300

    DepartmentDAO and SellerDAO interfaces

---
+++ src/model/dao/DepartmentDAO.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDAO {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Department obj);
+	Department findById(Integer id);
+	List<Department> findAll();
+}
---
+++ src/model/dao/SellerDAO.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDAO {
+	
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Seller obj);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+	
+}
